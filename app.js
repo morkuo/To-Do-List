7c2d782e15b7453b6e5bfb05415ce3a7161b768b
morkuo <80438073+morkuo@users.noreply.github.com>
2022-03-30 17:45:22 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-//取得element
-let submit = document.querySelector("button");
-let content = submit.parentElement.children[0];
-let month = submit.parentElement.children[1];
-let date = submit.parentElement.children[2];
+//取得新增按鈕、透過新增按鈕取得待辦事項填寫表單的各欄位
+let submit = document.querySelector("button.add");
+let content = submit.parentElement.children[1];
+let month = submit.parentElement.children[3];
+let date = submit.parentElement.children[5];
 
 //在按下新增按紐時，將待辦事項等資訊加入待辦清單
 submit.addEventListener("click", e => {
   //避免預設跳轉到指定網址
   e.preventDefault();
 
-  //如果未填寫待辦事項，跳出警告視窗要求填寫
+  //如果未填寫待辦事項，跳出警告視窗要求填寫，並停止執行
   if (content.value === "") {
-    alert("pls enter some text");
+    alert("請輸入待辦事項");
     return;
   }
 
-  //依照使用者填寫的資訊，建立事項、時間元素，把它們組合起來放進待辦清單
-  let sec = document.querySelector("section");
+  //依照使用者填寫的資訊，建立事項、時間元素，把它們組合起來放進待辦清單section
+  let sec = document.querySelector("section.listSec");
 
   let todo = document.createElement("div");
   todo.classList.add("todo");
@@ -44,38 +44,9 @@ submit.addEventListener("click", e => {
   todo.style.animation = "scaleUp 0.3s forwards";
 
   //清空待辦事項填寫表單的「事項」欄位
-  content.value = ""
+  content.value = "";
 
-  //如果就特定事項按下完成按紐，把該事項加上刪除線
-  completeButton.addEventListener("click", e => {
-    todo.classList.toggle("done");
-  })
-
-  //如果就特定事項按下刪除按紐，把該事項從待辦清單中移除，並從
-  deleteButton.addEventListener("click", e => {
-    //移除特定事項的動畫
-    todo.style.animation = "scaleDown 0.3s forwards";
-
-    //動畫完成後，移除此事項
-    todo.addEventListener("animationend", () => {
-      todo.remove();
-    })
-
-    let storeList = localStorage.getItem("list");
-    let arrayList = JSON.parse(storeList);
-    arrayList.forEach((item, index) => {
-      if (item.content == todoItem.innerText && item.month + "/" + item.date == todoTime.innerText) {
-        arrayList.splice(index, 1);
-      }
-    })
-    if (arrayList.length !== 0) {
-      localStorage.setItem("list", JSON.stringify(arrayList));
-    } else {
-      localStorage.removeItem("list");
-    }
-  })
-
-  //store in localStorage
+  //把待辦事項裝進物件，存進localStorage的待辦清單
   let storeList = localStorage.getItem("list");
   if (storeList === null) {
     let todoObject = {
@@ -95,37 +66,75 @@ submit.addEventListener("click", e => {
     arrayList.push(todoObject);
     localStorage.setItem("list", JSON.stringify(arrayList));
   }
+
+  //如果就特定事項按下完成按紐，把該事項加上 "done" class 讓css上刪除線
+  completeButton.addEventListener("click", e => {
+    todo.classList.toggle("done");
+  })
+
+  //如果就特定事項按下刪除按紐，把該事項從待辦清單中移除，並從LocalStorage刪除
+  deleteButton.addEventListener("click", e => {
+    //移除特定事項的動畫
+    todo.style.animation = "scaleDown 0.3s forwards";
+
+    //動畫完成後，移除此事項（div element）
+    todo.addEventListener("animationend", () => {
+      todo.remove();
+    })
+
+    //取得存在Localstorage裡的待辦清單，比對清單中各個事項的資訊跟此事項的資訊是否相同，如果相同，刪除清單裡的事項，重新存進Localstorage
+    let storeList = localStorage.getItem("list");
+    let arrayList = JSON.parse(storeList);
+    arrayList.forEach((item, index) => {
+      if (item.content == todoItem.innerText && item.month + "/" + item.date == todoTime.innerText) {
+        arrayList.splice(index, 1);
+      }
+    })
+    if (arrayList.length !== 0) {
+      localStorage.setItem("list", JSON.stringify(arrayList));
+    } else {
+      localStorage.removeItem("list");
+    }
+  })
 })
 
-//show list stored in localStorage when open browser
+//顯示待辦清單
 showList();
 
-//declare showList()
+//定義showList()
 function showList() {
   let storeList = localStorage.getItem("list");
+
+  //如果localstorage裡面的待辦清單不是空的，則用每個清單中物件資訊，建立事項、時間元素，把它們組合起來放進待辦清單section
   if (storeList !== null) {
     let arrayList = JSON.parse(storeList);
 
     arrayList.forEach(item => {
-      let sec = document.querySelector("section");
+      let sec = document.querySelector("section.listSec");
+
       let todo = document.createElement("div");
       todo.classList.add("todo");
+
       let todoItem = document.createElement("p");
       todoItem.classList.add("todoContent");
       todoItem.innerText = item.content;
+
       let todoTime = document.createElement("p");
       todoTime.classList.add("todoTime");
       todoTime.innerText = item.month + "/" + item.date;
 
+      let completeButton = document.createElement("button");
+      completeButton.innerText = "完成";
+      let deleteButton = document.createElement("button");
+      deleteButton.innerText = "移除";
+
+      sec.appendChild(todo);
       todo.appendChild(todoItem);
       todo.appendChild(todoTime);
-      sec.appendChild(todo);
-      todo.style.animation = "scaleUp 0.3s forwards";
-
-      let completeButton = document.createElement("button");
-      let deleteButton = document.createElement("button");
       todo.appendChild(completeButton);
       todo.appendChild(deleteButton);
+
+      todo.style.animation = "scaleUp 0.3s forwards";
 
       completeButton.addEventListener("click", e => {
         todo.classList.toggle("done");
@@ -151,32 +160,35 @@ function showList() {
   }
 }
 
-//sort by time
+//按時間排序
 let sort = document.querySelector("button.sort");
 sort.addEventListener("click", () => {
+
   storeList = localStorage.getItem("list");
   if (storeList !== null) {
     let arrayList = JSON.parse(storeList);
-    let sortedArray = mergeSort(arrayList, 0, arrayList.length - 1);
 
+    //用merge sort來排序，排完清單重新存進Localstorage
+    let sortedArray = mergeSort(arrayList, 0, arrayList.length - 1);
     localStorage.setItem("list", JSON.stringify(sortedArray));
 
-    let sec = document.querySelector("section");
-
-    //sec.children.length不可當作for loop condition，因為它會在for loop執行過程中變動
-    let len = sec.children.length
+    //透過一個for loop，把待辦清單section現有的事項都刪除（sec.children.length不可直接放進for loop condition，它會在for loop執行過程中變動）
+    let sec = document.querySelector("section.listSec");
+    let len = sec.children.length;
     for (let i = 0; i < len; i++) {
       sec.children[0].remove();
     }
 
+    //重新透過showlist()把待辦清單組出來，放進待辦清單section
     showList();
+
   } else {
     alert("nothing to sort");
   }
 })
 
 
-//merge sort (start sorting from index p to index q)
+//merge sort：把清單不斷地往下對半分，各剩下一個元素時，開始往回透過merge跟鄰近元素比較時間先後，然後重新向上拼湊成一個完整的清單。
 function mergeSort(arr, p, q) {
   let result = [];
   if (p < q) {
@@ -190,9 +202,10 @@ function mergeSort(arr, p, q) {
   }
 }
 
+//比較兩個部分各元素的時間先後，從時間在先的開始放入輔助陣列。其中一部分先處理完畢，則把另一部分也順著放進輔助陣列。
 function merge(arr1, arr2) {
   let auxArray = [];
-  let i = 0;
+  let i = 0;    
   let j = 0;
 
   while (i < arr1.length && j < arr2.length) {
@@ -213,6 +226,7 @@ function merge(arr1, arr2) {
     }
   }
 
+  //剩下的第一部分或第二部分順著放進輔助陣列
   while (i < arr1.length) {
     auxArray.push(arr1[i]);
     i++;
